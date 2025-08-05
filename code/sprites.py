1d5f67c3dcb0cb0dcea5de48d53ea4cc20478141
Ryan <yewryan@gmail.com>
2025-08-05 21:58:48 +0800

    clean and optimize level3 and sprites

--- code/sprites.py
+++ code/sprites.py
@@ -1,135 +1,149 @@
 import pygame
-from settings import *
+from settings import WINDOW_HEIGHT, WINDOW_WIDTH
 from random import choice, randint
 
+# --- Constants ---
+BG_SCROLL_SPEED = 300
+GROUND_SCROLL_SPEED = 360
+OBSTACLE_SCROLL_SPEED = 400
+JUMP_FORCE = -400
+GRAVITY = 600
+PLANE_ANIM_SPEED = 10
+
+PLANE_IMG_PATH = '../graphics/plane/red{}.png'
+BG_IMG_PATH = '../graphics/environment/background.png'
+GROUND_IMG_PATH = '../graphics/environment/ground.png'
+OBSTACLE_IMG_PATH = '../graphics/obstacles/{}.png'
+JUMP_SOUND_PATH = '../sounds/jump.wav'
+
 class BG(pygame.sprite.Sprite):
-    def __init__(self,groups,scale_factor):
-        super().__init__(groups)
-        bg_image = pygame.image.load('../graphics/environment/background.png').convert()
+    def __init__(self, *groups, scale_factor):
+        super().__init__(*groups)
+        self.sprite_type = 'background'
 
-        full_height = bg_image.get_height() * scale_factor
-        full_width = bg_image.get_width() * scale_factor
-        full_sized_image = pygame.transform.scale(bg_image,(full_width, full_height))
+        bg_image = pygame.image.load(BG_IMG_PATH).convert()
+        full_height = int(bg_image.get_height() * scale_factor)
+        full_width = int(bg_image.get_width() * scale_factor)
+        full_sized_image = pygame.transform.scale(bg_image, (full_width, full_height))
 
-        self.image = pygame.Surface((full_width * 2,full_height))
-        self.image.blit(full_sized_image,(0,0))
-        self.image.blit(full_sized_image,(full_width,0))
+        self.image = pygame.Surface((full_width * 2, full_height)).convert()
+        self.image.blit(full_sized_image, (0, 0))
+        self.image.blit(full_sized_image, (full_width, 0))
 
-        self.rect = self.image.get_rect(topleft = (0,0))
+        self.rect = self.image.get_rect(topleft=(0, 0))
         self.pos = pygame.math.Vector2(self.rect.topleft)
-
 
     def update(self, dt):
-        self.pos.x -= 300 * dt
+        self.pos.x -= BG_SCROLL_SPEED * dt
         if self.rect.centerx <= 0:
             self.pos.x = 0
-        self.rect.x = round(self.pos.x)
+        self.rect.x = int(self.pos.x)
+
 
 class Ground(pygame.sprite.Sprite):
-    def __init__(self,groups,scale_factor):
-        super().__init__(groups)
+    def __init__(self, *groups, scale_factor):
+        super().__init__(*groups)
         self.sprite_type = 'ground'
 
-        # image
-        ground_surf = pygame.image.load('../graphics/environment/ground.png').convert_alpha()
-        self.image = pygame.transform.scale(ground_surf, pygame.math.Vector2(ground_surf.get_size()) * scale_factor)
+        ground_surf = pygame.image.load(GROUND_IMG_PATH).convert_alpha()
+        self.image = pygame.transform.scale(
+            ground_surf,
+            pygame.math.Vector2(ground_surf.get_size()) * scale_factor
+        )
 
-        # position
-        self.rect = self.image.get_rect(bottomleft = (0, WINDOW_HEIGHT))
+        self.rect = self.image.get_rect(bottomleft=(0, WINDOW_HEIGHT))
         self.pos = pygame.math.Vector2(self.rect.topleft)
-
-        #mask
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self, dt):
-        self.pos.x -= 360 * dt
+        self.pos.x -= GROUND_SCROLL_SPEED * dt
         if self.rect.centerx <= 0:
             self.pos.x = 0
+        self.rect.x = int(self.pos.x)
 
-        self.rect.x = round(self.pos.x)
 
 class Plane(pygame.sprite.Sprite):
-    def __init__(self,groups,scale_factor):
-        super().__init__(groups)
+    def __init__(self, *groups, scale_factor):
+        super().__init__(*groups)
+        self.sprite_type = 'player'
 
-        #image
+        self.frames = []
         self.import_frames(scale_factor)
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
 
-        #rect
-        self.rect = self.image.get_rect(midleft = (WINDOW_WIDTH/20,WINDOW_HEIGHT/2))
+        self.rect = self.image.get_rect(midleft=(WINDOW_WIDTH / 20, WINDOW_HEIGHT / 2))
         self.pos = pygame.math.Vector2(self.rect.topleft)
-
-        #movement
-        self.gravity = 600
+        self.gravity = GRAVITY
         self.direction = 0
-
-        #mask
         self.mask = pygame.mask.from_surface(self.image)
 
-        #sound
-        self.jump_sound = pygame.mixer.Sound('../sounds/jump.wav')
+        self.jump_sound = pygame.mixer.Sound(JUMP_SOUND_PATH)
         self.jump_sound.set_volume(0.3)
 
-    def import_frames(self,scale_factor):
-        self.frames = []
+    def import_frames(self, scale_factor):
         for i in range(3):
-            surf = pygame.image.load(f'../graphics/plane/red{i}.png').convert_alpha()
-            scaled_surface = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scale_factor)
-            self.frames.append(scaled_surface)
+            surf = pygame.image.load(PLANE_IMG_PATH.format(i)).convert_alpha()
+            scaled_surf = pygame.transform.scale(
+                surf,
+                pygame.math.Vector2(surf.get_size()) * scale_factor
+            )
+            self.frames.append(scaled_surf)
 
-    def apply_gravity(self,dt):
+    def apply_gravity(self, dt):
         self.direction += self.gravity * dt
         self.pos.y += self.direction * dt
-        self.rect.y = round(self.pos.y)
+        self.rect.y = int(self.pos.y)
 
     def jump(self):
         self.jump_sound.play()
-        self.direction =- 400
+        self.direction = JUMP_FORCE
 
-    def animate(self,dt):
-        self.frame_index += 10 * dt
+    def animate(self, dt):
+        self.frame_index += PLANE_ANIM_SPEED * dt
         if self.frame_index >= len(self.frames):
             self.frame_index = 0
         self.image = self.frames[int(self.frame_index)]
 
     def rotate(self):
-        rotated_plane = pygame.transform.rotozoom(self.image, -self.direction * 0.06,1)
-        self.image =  rotated_plane
+        rotated = pygame.transform.rotozoom(self.image, -self.direction * 0.06, 1)
+        self.image = rotated
         self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self,dt):
+    def update(self, dt):
         self.apply_gravity(dt)
         self.animate(dt)
         self.rotate()
 
+
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self,groups,scale_factor):
-        super().__init__(groups)
+    def __init__(self, *groups, scale_factor):
+        super().__init__(*groups)
         self.sprite_type = 'obstacle'
 
-        orientation = choice (('up','down'))
-        surf = pygame.image.load(f'../graphics/obstacles/{choice((0,1))}.png').convert_alpha()
-        self.image = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scale_factor)
+        orientation = choice(('up', 'down'))
+        sprite_index = choice((0, 1))
+        surf = pygame.image.load(OBSTACLE_IMG_PATH.format(sprite_index)).convert_alpha()
 
-        x = WINDOW_WIDTH + randint(40,100)
+        self.image = pygame.transform.scale(
+            surf,
+            pygame.math.Vector2(surf.get_size()) * scale_factor
+        )
 
+        x = WINDOW_WIDTH + randint(40, 100)
         if orientation == 'up':
-            y = WINDOW_HEIGHT + randint(10,50)
-            self.rect = self.image.get_rect(midbottom = (x,y))
+            y = WINDOW_HEIGHT + randint(10, 50)
+            self.rect = self.image.get_rect(midbottom=(x, y))
         else:
             y = randint(-50, -10)
             self.image = pygame.transform.flip(self.image, False, True)
-            self.rect = self.image.get_rect(midtop = (x,y))
+            self.rect = self.image.get_rect(midtop=(x, y))
 
         self.pos = pygame.math.Vector2(self.rect.topleft)
-
-        #mask
         self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self,dt):
-        self.pos.x -= 400 * dt
-        self.rect.x = round(self.pos.x)
+    def update(self, dt):
+        self.pos.x -= OBSTACLE_SCROLL_SPEED * dt
+        self.rect.x = int(self.pos.x)
         if self.rect.right <= -100:
             self.kill()
